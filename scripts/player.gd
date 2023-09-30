2594d22b02f9e9beeb9adf0ccb46326f3b6c2990
Dan McKinley <danmckinley54@gmail.com>
2023-09-30 20:39:05 +0100

    committing small changes

--- scripts/player.gd
+++ scripts/player.gd
@@ -46,21 +46,6 @@ func set_move_speed_based_on_health():
 		move_speed_modifier = 0.5
 	move_speed_modifier = clamp(move_speed_modifier, 0.5, 1)
 	move_speed = _max_move_speed * move_speed_modifier
-
-#OLD
-	# Add the gravity.
-	#if not is_on_floor():
-	#	velocity.y += gravity * delta
-	# Handle Jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-	#	velocity.y = JUMP_VELOCITY
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	#var direction = Input.get_maxis("ui_left", "ui_right")
-	#if direction:
-	#	velocity.x = direction * SPEED
-	#else:
-	#	velocity.x = move_toward(velocity.x, 0, SPEED)
 	
 func toggle_aim():
 	# unholtser gun + aim in one action
@@ -68,10 +53,16 @@ func toggle_aim():
 		is_aiming= false
 	is_aiming = true
 	
-
 #func fire_action():	
 	# if aiming
 		# fire gun
 	# else
 		# push action
 		# grab
+		
+#func push():
+	# push enemies back, 
+	# if you push too much, get tired for a few seconds
+	
+#func grab(pass object):
+	# grab
