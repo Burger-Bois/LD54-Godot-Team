f4dcbe039bd01d1f585c4077bfbb1c79e6540813
Ewan Fenwick <racfairfax@gmail.com>
2023-09-30 14:13:37 +0200

    added player controller

---
+++ scripts/Player.gd
@@ -0,0 +1,29 @@
+extends CharacterBody2D
+
+class_name Player
+
+# Movement speed in pixels per second.
+@export var speed := 500
+
+#onready var animated_sprite: AnimatedSprite = $AnimatedSprite
+
+
+func _physics_process(_delta: float) -> void:
+	# Once again, we call `Input.get_action_strength()` to support analog movement.
+	var direction := Vector2(
+		# This first line calculates the X direction, the vector's first component.
+		Input.get_action_strength("right") - Input.get_action_strength("left"),
+		# And here, we calculate the Y direction. Note that the Y-axis points 
+		# DOWN in games.
+		# That is to say, a Y value of `1.0` points downward.
+		Input.get_action_strength("down") - Input.get_action_strength("up")
+	)
+	# When aiming the joystick diagonally, the direction vector can have a length 
+	# greater than 1.0, making the character move faster than our maximum expected
+	# speed. When that happens, we limit the vector's length to ensure the player 
+	# can't go beyond the maximum speed.
+	if direction.length() > 1.0:
+		direction = direction.normalized()
+		
+	velocity = (speed * direction)
+	move_and_slide()
