f98bcb8fc0671451795b3085654dab3d12f5f17d
Ewan Fenwick <racfairfax@gmail.com>
2023-09-30 17:51:44 +0200

    added obsticles

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -7,13 +7,19 @@ var accel = 7
 
 @onready var nav: NavigationAgent2D = $NavigationAgent2D
 
+func _ready() -> void:
+	var callable = Callable(self, "move")
+	nav.connect("velocity_computed", callable)
+
 func _physics_process(delta):
 	var direction = Vector3()
 	
 	nav.target_position = target.global_position
 	
-	direction = nav.get_next_path_position() - global_position
-	direction = direction.normalized()
+	direction = (nav.get_next_path_position() - global_position).normalized()
 	
-	velocity = velocity.lerp(direction * speed, accel* delta)
+	nav.velocity = velocity.lerp(direction * speed, accel* delta)
+
+func move(velo: Vector2) -> void:
+	velocity = velo
 	move_and_slide()
