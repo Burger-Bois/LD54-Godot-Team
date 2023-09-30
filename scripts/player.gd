a35f882b40c0dca378783f19164105ace65b70d3
Daniel McKinley <danmckinley54@gmail.com>
2023-09-30 21:05:52 +0100

    Resized player collision & sprite

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,9 +2,9 @@ extends CharacterBody2D
 class_name Player
 
 # consts / exposed vars
-const _max_move_speed = 300.0
-const _max_health = 100
-const _max_rotation_speed = 7
+@export var _max_move_speed = 150.0
+@export var _max_health = 100
+@export var _max_rotation_speed = 7
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 #var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
