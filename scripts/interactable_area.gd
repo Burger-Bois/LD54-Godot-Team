a708fcaa3631ab4d873b34ca558012debd5ef929
John Stones <john.stones@protonmail.com>
2023-10-02 16:02:51 +0100

    fix placing and grabbing at the same time

--- scripts/interactable_area.gd
+++ scripts/interactable_area.gd
@@ -5,15 +5,6 @@ var action_state = "off"
 
 var obstacle_manager: ObstacleManager
 
-func _process(_delta):
-	if Input.is_action_just_pressed("interact"):
-		match action_state:
-			"off":
-				pass
-			"interactable":
-				if not obstacle_manager.carrying_obstacle:
-					obstacle_manager.interact_with_tile(global_position, self)
-
 func delete_self():
 	queue_free()
 
--- scripts/obstacle_manager.gd
+++ scripts/obstacle_manager.gd
@@ -3,6 +3,8 @@ class_name ObstacleManager
 
 signal carrying_obstacle_changed(value: bool)
 
+const OBSTACLE_TILE_ID := 3
+
 @export var tilemap: TileMap
 @export var interactableArea: PackedScene
 
@@ -12,12 +14,21 @@ var carrying_obstacle = false:
 		carrying_obstacle_changed.emit(value)
 		carrying_obstacle = value
 
+var _obstacle_area_map := {} as Dictionary # Dictionary[Vectori, InteractableArea]
+
 func _process(_delta):
-	if Input.is_action_just_pressed("interact") && carrying_obstacle:
+	if Input.is_action_just_pressed("interact"):
+		var clicked_tile_coords := tilemap.local_to_map(get_local_mouse_position())
+		var clicked_tile_id := tilemap.get_cell_source_id(0, clicked_tile_coords)
+		
 		if carrying_obstacle:
-			var clicked_tile = tilemap.local_to_map(get_local_mouse_position())
-			set_and_save_tile(clicked_tile, 3, tilemap.get_cell_source_id(0, clicked_tile))
+			set_and_save_tile(clicked_tile_coords, OBSTACLE_TILE_ID, clicked_tile_id)
 			carrying_obstacle = false
+		
+		elif clicked_tile_id == OBSTACLE_TILE_ID:
+			var obstacle_area := _obstacle_area_map[clicked_tile_coords] as InteractableArea
+			if obstacle_area.action_state == "interactable":
+				interact_with_tile(obstacle_area)
 	
 ##set tile method that takes in the position and what the tile was
 #save that to an array of tile datas
@@ -30,20 +41,21 @@ func set_and_save_tile(pos: Vector2i, new_tile_ID: int, old_tile_ID: int):
 	var new_area = interactableArea.instantiate() as InteractableArea
 	new_area.obstacle_manager = self
 	new_area.global_position = tilemap.map_to_local(pos)
+	_obstacle_area_map[pos] = new_area
 	call_deferred("add_child", new_area)
 	
 func set_tile(pos: Vector2i, new_tile_ID: int):
 	tilemap.set_cell(0, pos, new_tile_ID, Vector2i(0,0))
 
 ##restoresavedtile method that takes in position and returns the tile to its original form
-func interact_with_tile(pos: Vector2, area: InteractableArea):
+func interact_with_tile(area: InteractableArea):
 	if carrying_obstacle:
 		return 
 	else:
 		carrying_obstacle = true
 	
 	##set tile at pos to old tile
-	var cellposition = tilemap.local_to_map(pos)
+	var cellposition = tilemap.local_to_map(area.global_position)
 	
 	for n in saved_tiles_array:
 		if n.position == cellposition:
